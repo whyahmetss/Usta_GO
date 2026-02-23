386e0e50a829aaa5dab0dea8fad0b743558fcecc
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-24 01:32:12 +0300

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom' // useLocation burada MUTLAKA olmalı!
 import { useAuth } from '../context/AuthContext'
 
 
