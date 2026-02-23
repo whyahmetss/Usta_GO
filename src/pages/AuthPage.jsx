a1d2ce540125d38ad420d280348c08fae51efb9c
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-24 01:21:25 +0300

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { useState, useEffect } from 'react' // useEffect ekledik
-import { useNavigate, useLocation } from 'react-router-dom' // useLocation ekledik
+
 
 function AuthPage() {
   const [isLogin, setIsLogin] = useState(true)
