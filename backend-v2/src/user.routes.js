406b38fbe73b54cc52a8d2762f5b7906e9ff589d
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-23 21:21:17 +0300

    Delete backend-v2/src/user.routes.js

--- backend-v2/src/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as userController from "../controllers/user.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
-
-const router = Router();
-// Frontend'in aradığı adres: /api/upload/photo
-router.post("/upload/photo", authMiddleware, userController.uploadPhoto);
-
-export default router;
