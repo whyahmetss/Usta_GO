1c14fe56d897a57759311bbf9bb0e5825dace43f
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-24 01:22:07 +0300

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
-import { useState, useEffect } from 'react' // useEffect ekledik
+
 
 
 function AuthPage() {
