189598176077984a7781e904a495803e238cce16
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-24 17:43:39 +0300

    Update HamburgerMenu.jsx

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -101,7 +101,7 @@ function HamburgerMenu({ isOpen, onClose }) {
           <p className="text-center text-xs text-gray-500">
             Usta Go v3.0.0
             <br />
-            © 2026 Tüm hakları saklıdır
+            © 2025 Tüm hakları saklıdır
           </p>
         </div>
       </div>
