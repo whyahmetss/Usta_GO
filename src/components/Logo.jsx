52a57bf0cdc9bed1dcd874038c2d0401267cd403
whyahmetss <113728812+whyahmetss@users.noreply.github.com>
2026-02-24 23:04:42 +0300

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,10 +5,10 @@ import logoAdmin from '../assets/logo-admin.png'
 import logoDefault from '../assets/logo-default.png'
 
 const sizeMap = {
-    xs: 'w-12 h-12',    // Eskiden w-8 idi, artık daha görünür
-    sm: 'w-16 h-16',    // Biraz daha büyüdü
-    md: 'w-24 h-24',    // Standart boyutu ciddi oranda artırdık
-    lg: 'w-32 h-32',    // Büyük logolar için
+    xs: 'w-8 h-8',    // Eskiden w-8 idi, artık daha görünür
+    sm: 'w-14 h-14',    // Biraz daha büyüdü
+    md: 'w-16 h-16',    // Standart boyutu ciddi oranda artırdık
+    lg: 'w-24 h-24',    // Büyük logolar için
     xl: 'w-48 h-48',    // Ana sayfa veya dev ekranlar için
   }
 
